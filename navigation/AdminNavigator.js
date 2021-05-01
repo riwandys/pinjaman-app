b1e2b05b4e334dd88110274cdea0fd29fd9c19ea
Riwandy <riwandys@gmail.com>
2021-05-01 18:16:02 +0700

    Create Admin Navigator

---
+++ navigation/AdminNavigator.js
@@ -0,0 +1,19 @@
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import AdminHomeScreen from '../screens/AdminHomeScreen';
+import UserDetailScreen from '../screens/UserDetailScreen';
+import AcceptApplicationScreen from '../screens/AcceptApplicationScreen';
+import RejectApplicationAndPaymentScreen from '../screens/RejectApplicationAndPaymentScreen';
+import PaymentDetailScreen from '../screens/PaymentDetailScreen';
+import AdminTransactionHistoryScreen from '../screens/AdminTransactionHistoryScreen';
+
+const AdminNavigator = createStackNavigator({
+    AdminHome: AdminHomeScreen,
+    UserDetail: UserDetailScreen,
+    AcceptApplication: AcceptApplicationScreen,
+    RejectApplicationAndPayment: RejectApplicationAndPaymentScreen,
+    PaymentDetail: PaymentDetailScreen,
+
+});
+
+export default createAppContainer(AdminNavigator);
