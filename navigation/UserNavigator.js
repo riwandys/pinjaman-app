fc1e22025a2230afad38f51382d0307bdd62e278
Riwandy <riwandys@gmail.com>
2021-05-01 15:10:44 +0700

    Add UserNavigator

---
+++ navigation/UserNavigator.js
@@ -0,0 +1,25 @@
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import UserHomeScreen from "../screens/UserHomeScreen";
+import LoanApplyScreen from "../screens/LoanApplyScreen";
+import LoanConfirmationScreen from "../screens/LoanConfirmationScreen";
+import TransactionListScreen from "../screens/TransactionListScreen";
+import PayBillScreen from '../screens/PayBillScreen';
+import PaymentConfirmationScreen from '../screens/PaymentConfirmationScreen';
+import UserTransactionHistoryScreen from '../screens/UserTransactionHistoryScreen';
+import AccountScreen from '../screens/AccountScreen';
+import SettingScreen from '../screens/SettingScreen';
+
+const UserNavigator = createStackNavigator({
+    UserHome: UserHomeScreen,
+    LoanApply: LoanApplyScreen,
+    LoanConfirmation: LoanConfirmationScreen,
+    TransactionList: TransactionListScreen,
+    PayBill: PayBillScreen,
+    PaymentConfirmation: PaymentConfirmationScreen,
+    UserTransactionHistory: UserTransactionHistoryScreen,
+    Account: AccountScreen,
+    Setting: SettingScreen
+});
+
+export default createAppContainer(UserNavigator);
