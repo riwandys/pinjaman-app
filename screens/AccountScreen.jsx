6dea80b4a13ebe09602d0d581e1972227d39cc88
Tasya Karinda <edrakemud22@gmail.com>
2021-05-04 04:42:19 +0700

    Update AccountScreen.jsx

--- screens/AccountScreen.jsx
+++ screens/AccountScreen.jsx
@@ -1,1 +1,35 @@
- 
+import React from 'react'
+import { Text, View, TextInput, Button, SafeAreaView } from 'react-native'
+
+export default function AccountScreen(){
+    return (
+        <View>
+            <View style={styles.container}>
+                <Text style={styles.text1}>
+                    Halo!
+                    Nama Pengguna
+                </Text>
+                <Text style={styles.text2}>
+                    DD/MM/YY
+                </Text>
+            </View>
+            <View style={styles.limit}>
+                <Text style={styles.textlimit}>
+                    Limit Pinjam
+                    <Text style={styles.nominal}>
+                        Rp.XXX.XXX
+                    </Text>
+                </Text>
+                <Text style={styles.textlimit}>
+                    Sisa Limit
+                    <Text style={styles.nominal}>
+                        Rp.XXX.XXX
+                    </Text>
+                </Text>
+            </View>
+            <View style={styles.button}>
+                <Button title="Pinjam Sekarang"></Button>
+            </View>
+        </View>
+    )
+}
