7654620e5f560e4bd4dff200cd43f8c81d56aef8
Tasya Karinda <edrakemud22@gmail.com>
2021-05-04 06:13:43 +0700

    Update RejectApplicationAndPaymentScreen.jsx

--- screens/RejectApplicationAndPaymentScreen.jsx
+++ screens/RejectApplicationAndPaymentScreen.jsx
@@ -1,1 +1,31 @@
- 
+import React from 'react'
+import { View, Button, Text, TextInput } from 'react-native'
+
+export default function RejectApplicationAndPaymentScreen(){
+    return(
+        <View>
+            <View style={styles.header}>
+                <Text>
+                    Tolak Pengajuan
+                    <Button title="<"></Button>
+                </Text>
+            </View>
+            <View style={style.container}>
+                <Text>
+                    Alasan Penolakan Pengajuan Pinjaman
+                    <TextInput style={styles.input}></TextInput>
+                </Text>
+            </View>
+            <View style={styles.footer}>
+                <Button title="Tolak Pengajuan"></Button>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {},
+    container: {},
+    input: {},
+    footer: {}
+})
