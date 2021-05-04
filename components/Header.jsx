3b437d2f954a857a66f345f0457611f3a1a01c3a
Riwandy <riwandys@gmail.com>
2021-05-05 00:28:31 +0700

    Create View for LoginScreen

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar, StyleSheet, Text, View } from 'react-native'
 
 export default function Header() {
     return (
         <View style={styles.container}>
-            <Text>PinjaMAN</Text>
+            <Text style={styles.headerText}>PinjaMAN</Text>
         </View>
     )
 }
@@ -13,7 +13,14 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#F49135',
         width: '100%',
-        height: 47,
-        marginBottom: 100
+        paddingTop: StatusBar.currentHeight,
+        height: StatusBar.currentHeight + 54,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    headerText: {
+        color: 'white',
+        fontSize: 15,
+        fontWeight: 'bold'
     }
 });
--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,14 +1,71 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { KeyboardAvoidingView, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, TextInput, TouchableNativeFeedback, View } from 'react-native'
 import Header from '../components/Header';
-import LoginForm from '../components/LoginForm'
+import Logo from '../assets/logo.svg';
 
 const LoginScreen = () => {
-    return (<>
-        <Header />
-        <LoginForm />
-    </>
-    );
+    return (
+        <SafeAreaView>
+            <StatusBar backgroundColor="rgba(0,0,0,0.1)" translucent />
+            <Header title="PinjaMAN" />
+            <ScrollView style={styles.scrollContainer}>
+                <KeyboardAvoidingView behavior='position'>
+                    <View style={styles.container}>
+                        <Logo style={styles.logo} />
+                        <View style={styles.formContainer}>
+                            <TextInput style={{ ...styles.textInput, marginBottom: 10 }} placeholder="Email" textContentType='emailAddress' keyboardType='email-address' />
+                            <TextInput style={{ ...styles.textInput, marginBottom: 44 }} placeholder="PIN" secureTextEntry keyboardType='number-pad' />
+                            <TouchableNativeFeedback useForeground={true}>
+                                <View style={{ ...styles.button, backgroundColor: '#343434', marginBottom: 12 }}>
+                                    <Text style={{ color: 'white' }}>MASUK</Text>
+                                </View>
+                            </TouchableNativeFeedback>
+                            <TouchableNativeFeedback useForeground={true}>
+                                <View style={{ ...styles.button, backgroundColor: 'white' }}>
+                                    <Text style={{ color: '#F49135' }}>DAFTAR</Text>
+                                </View>
+                            </TouchableNativeFeedback>
+                        </View>
+                    </View>
+                </KeyboardAvoidingView>
+            </ScrollView>
+        </SafeAreaView >
+    )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    logo: {
+        marginTop: 115
+    },
+    formContainer: {
+        marginTop: 72,
+        paddingHorizontal: 34,
+        paddingTop: 52,
+        paddingBottom: 40,
+        backgroundColor: '#F49135',
+        width: 285,
+        borderRadius: 20
+    },
+    textInput: {
+        width: '100%',
+        height: 38,
+        backgroundColor: 'white',
+        borderRadius: 3,
+        paddingHorizontal: 12
+    },
+    button: {
+        width: '100%',
+        height: 38,
+        borderRadius: 3,
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 4
+    }
+})
 
 export default LoginScreen;
