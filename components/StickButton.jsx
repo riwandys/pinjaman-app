27dfe51404abf5a4212e8a1491ac1419396df0fe
Riwandy <riwandys@gmail.com>
2021-05-08 23:12:10 +0700

    Create Component StickButton.jsx

---
+++ components/StickButton.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { StyleSheet, Text, View, TouchableNativeFeedback } from 'react-native'
+import color from '../constants/color';
+
+const StickButton = (props) => {
+    let backgroundColor, textColor;
+
+    if (props.secondary) {
+        backgroundColor = '#F3F3F3';
+        textColor = 'black';
+    } else {
+        backgroundColor = color.primary;
+        textColor = 'white';
+    }
+
+    return (
+        <TouchableNativeFeedback>
+            <View style={{ ...styles.stickButton, backgroundColor }}>
+                <Text style={{ ...styles.buttonText, color: textColor }}>{props.text}</Text>
+            </View>
+        </TouchableNativeFeedback>
+    )
+}
+
+export default StickButton
+
+const styles = StyleSheet.create({
+    stickButton: {
+        height: 60,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        fontSize: 14,
+        fontWeight: 'bold'
+    }
+})
