0342d5e3b40f97847590cc4aa59b0637589ab187
Riwandy <riwandys@gmail.com>
2021-05-08 18:36:48 +0700

    Create view for AddNewAdminScreen

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,10 +3,14 @@ import { StatusBar, StyleSheet, Text, View } from 'react-native';
 import color from '../constants/color';
 
 export default function Header(props) {
+    const LeftIcon = props.leftIcon || View;
+    const RightIcon = props.rightIcon || View;
     return (<>
         <StatusBar backgroundColor="rgba(0,0,0,0.1)" translucent />
         <View style={styles.container}>
+            <LeftIcon width={24} height={24} />
             <Text style={styles.headerText}>{props.title}</Text>
+            <RightIcon width={24} height={24} />
         </View>
     </>
     )
@@ -17,9 +21,11 @@ const styles = StyleSheet.create({
         backgroundColor: color.primary,
         width: '100%',
         paddingTop: StatusBar.currentHeight,
+        paddingHorizontal: 16,
         height: StatusBar.currentHeight + 54,
+        flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'space-between'
     },
     headerText: {
         color: 'white',
--- screens/AddNewAdminScreen.jsx
+++ screens/AddNewAdminScreen.jsx
@@ -1,18 +1,56 @@
 import React from 'react'
-import { StyleSheet, Text, View, StatusBar, SafeAreaView, ScrollView, TextInput } from 'react-native'
-import Header from '../components/Header'
+import { StyleSheet, Text, View, StatusBar, SafeAreaView, ScrollView, TextInput, TouchableNativeFeedback, KeyboardAvoidingView } from 'react-native'
+import Header from '../components/Header';
+import BackIcon from '../assets/back_icon.svg'
+import color from '../constants/color';
 
 const AddNewAdminScreen = () => {
     return (
-        <SafeAreaView>
-            <Header title="Tambah Admin" />
-            <ScrollView style={styles.scrollContainer}>
-                <TextInput />
+        <SafeAreaView style={styles.container}>
+            <Header title="Tambah Admin" leftIcon={BackIcon} />
+            <ScrollView style={styles.formContainer}>
+                <TextInput style={{ ...styles.textInput, marginBottom: 10 }} placeholder="Email" textContentType='emailAddress' keyboardType='email-address' />
+                <TextInput style={styles.textInput} placeholder="Password" secureTextEntry keyboardType='default' />
             </ScrollView>
+            <KeyboardAvoidingView behavior='height'>
+                <TouchableNativeFeedback>
+                    <View style={styles.bottomButton}>
+                        <Text style={styles.buttonText}>Tambah</Text>
+                    </View>
+                </TouchableNativeFeedback>
+            </KeyboardAvoidingView>
         </SafeAreaView >
     )
 }
 
 export default AddNewAdminScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    formContainer: {
+        padding: 16
+    },
+    textInput: {
+        width: '100%',
+        height: 38,
+        backgroundColor: 'white',
+        borderRadius: 3,
+        paddingHorizontal: 12,
+        borderColor: '#C1C1C1',
+        borderWidth: 1
+    },
+    bottomButton: {
+        backgroundColor: color.primary,
+        height: 60,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 14,
+        fontWeight: 'bold'
+    }
+})
